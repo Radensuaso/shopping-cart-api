8f87d6fbbf6d50b80a4f433fc1586543e4f5ae01
Radensuaso <andrels9283@gmail.com>
2021-08-26 14:43:23 +0100

    Defined relationships between tables

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -4,11 +4,47 @@ import User from "./User.js";
 import Review from "./Review.js";
 import Cart from "./Cart.js";
 
+//============== Relationship between Product and category ==============
+Category.hasMany(Product, {
+  onDelete: "cascade",
+  foreignKey: { allowNull: false },
+});
 Product.belongsTo(Category, {
   onDelete: "cascade",
   foreignKey: { allowNull: false },
 });
-Category.hasMany(Product, {
+
+//============== Relationship between Product User and Cart ==============
+Product.belongsToMany(User, {
+  through: { model: Cart, unique: false },
+  foreignKey: { allowNull: false },
+});
+User.belongsToMany(Product, {
+  through: { model: Cart, unique: false },
+  foreignKey: { allowNull: false },
+}); //unique: false => to prevent creating primary key
+
+Product.hasMany(Cart); // Product.findAll({include: Cart})
+Cart.belongsTo(Product); // Cart.findAll({include: Product})
+
+User.hasMany(Cart); // User.findAll({include: Cart})
+Cart.belongsTo(User); // Cart.findAll({include: User})
+
+//================= Relationship between Product, Reviews and Users ==============
+Product.hasMany(Review, {
+  onDelete: "cascade",
+  foreignKey: { allowNull: false },
+});
+Review.belongsTo(Product, {
+  onDelete: "cascade",
+  foreignKey: { allowNull: false },
+});
+
+User.hasMany(Review, {
+  onDelete: "cascade",
+  foreignKey: { allowNull: false },
+});
+Review.belongsTo(User, {
   onDelete: "cascade",
   foreignKey: { allowNull: false },
 });
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import { syncSequelize } from "./db/index.js";
 import cors from "cors";
 import categoriesRouter from "./services/categories/index.js";
 import productsRouter from "./services/products/index.js";
+import usersRouter from "./services/users/index.js";
+import reviewsRouter from "./services/reviews/index.js";
+import cartRouter from "./services/cart/index.js";
 
 const app = express();
 
@@ -13,6 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/categories", categoriesRouter);
 app.use("/products", productsRouter);
+app.use("/users", usersRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/cart", cartRouter);
 
 app.listen(PORT, async () => {
   try {
